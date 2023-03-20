aa049e74d67715b5d5bf44ed0e2608f1a0366605
kaye kinser <kaly@kinser.us>
2023-03-20 03:41:58 -0500

    added a footer

--- knots-by-kaye-react/src/footer.js
+++ knots-by-kaye-react/src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="Footer">
+        <div className="Footer text-center">
             Coded by Kaye Kinser, open-sourced on <a href="https://github.com/KayeBK/knots-by-kaye-react" target="_blank" rel="noreferrer">GitHub</a>, hosted on <a href="/" target="_blank" rel="noreferrer">Netlify</a>.
         </div>
     )
