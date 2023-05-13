38145c44d4d96470fef2819f81825db2ef2ebc79
kaye kinser <kaly@kinser.us>
2023-05-13 15:13:02 -0500

    Added images

--- knots-by-kaye-react/src/pages/card.js
+++ knots-by-kaye-react/src/pages/card.js
@@ -10,7 +10,8 @@ const Card = ({ item }) => {
                     return (
                         <div className="col-3 card-items shadow" key={Val.id}>
                             <div className="card-img-top text-center img-card">
-                                <img src={Val.img} alt={Val.title} className="w-75" />
+                            <a href={Val.img} target="_blank" rel="noreferrer">
+                            <img src={Val.img} alt={`${Val.title} IMG COMING SOON`} className="w-100 card-img" /> </a>
                             </div>
                             <div className="card-body">
                                 <div className="card-text card-title">{Val.title} | <span className="card-price">{Val.price}</span></div>
